27b78091ba14ebdb6ca55a555140206b7e8abd11
Skylar Wilcox <lenin1916@gmail.com>
2018-03-13 09:40:36 -0700

    remove dummy homescreen from Navigation

--- App.js
+++ App.js
@@ -3,16 +3,6 @@ import { StyleSheet, Text, View } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 import { CalendarScreen } from './src/components/CalendarScreen.js'
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen!!</Text>
-      </View>
-    )
-  }
-}
-
 export default StackNavigator(
   {
     Calendar: {
